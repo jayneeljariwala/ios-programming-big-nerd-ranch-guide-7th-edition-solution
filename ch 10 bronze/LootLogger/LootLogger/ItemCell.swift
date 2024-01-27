d5247e9414338eb1acdd073f90091ddd999c3d5e
jayneeljariwala <jayneel2001@gmail.com>
2024-01-27 15:00:02 +0530

    Completed chapter 10 bronze challenge

---
+++ ch 10 bronze/LootLogger/LootLogger/ItemCell.swift
@@ -0,0 +1,17 @@
+//
+//  ItemCell.swift
+//  LootLogger
+//
+//  Created by Jayneel Jariwala on 25/01/24.
+//
+
+import UIKit
+
+class ItemCell: UITableViewCell {
+    
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var serialNumberLabel: UILabel!
+    @IBOutlet weak var valueLabel: UILabel!
+    
+    
+}
