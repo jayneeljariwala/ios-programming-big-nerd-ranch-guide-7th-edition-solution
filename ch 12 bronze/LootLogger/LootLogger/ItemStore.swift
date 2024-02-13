aa12f11f847e7097e386b54a71108899e7818f53
jayneeljariwala <jayneel2001@gmail.com>
2024-02-13 19:22:32 +0530

    Completed chapter 12 bronze challenge

---
+++ ch 12 bronze/LootLogger/LootLogger/ItemStore.swift
@@ -0,0 +1,35 @@
+//
+//  ItemStore.swift
+//  LootLogger
+//
+//  Created by Jayneel Jariwala on 12/01/24.
+//
+
+import UIKit
+
+class ItemStore {
+    var allItems = [Item]()
+    
+    @discardableResult func createItem() -> Item {
+        let newItem = Item(random: true)
+        allItems.append(newItem)
+        return newItem
+    }
+    
+    func removeItem(_ item: Item) {
+        if let index = allItems.firstIndex(of: item) {
+            allItems.remove(at: index)
+        }
+    }
+    
+    func moveItem(from fromIndex: Int, to toIndex: Int) {
+        if fromIndex == toIndex {
+            return
+        }
+        
+        let movedItem = allItems[fromIndex]
+        
+        allItems.remove(at: fromIndex)
+        allItems.insert(movedItem, at: toIndex)
+    }
+}
