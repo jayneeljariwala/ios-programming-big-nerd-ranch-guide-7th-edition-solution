cc24f874e6cc488589478efa5c090045cac3f11f
jayneeljariwala <jayneel2001@gmail.com>
2024-02-20 15:10:55 +0530

    Conpleted chapter 14

---
+++ ch 14/LootLogger/LootLogger/DetailViewController.swift
@@ -0,0 +1,103 @@
+//
+//  DetailViewController.swift
+//  LootLogger
+//
+//  Created by Jayneel Jariwala on 07/02/24.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController, UITextFieldDelegate {
+
+    @IBOutlet weak var nameField: UITextField!
+    @IBOutlet weak var serialNumberField: UITextField!
+    @IBOutlet weak var valueField: UITextField!
+    @IBOutlet weak var dateLabel: UILabel!
+    
+    @IBAction func backgroundTapped(_ sender: UITapGestureRecognizer) {
+        view.endEditing(true)
+    }
+    
+    @IBAction func choosePhotoSource(_ sender: UIBarButtonItem) {
+        let alertController = UIAlertController(title: nil, message: nil, preferredStyle: .actionSheet)
+        
+        alertController.modalPresentationStyle = .popover
+        
+        let cameraAction = UIAlertAction(title: "Camera", style: .default) { _ in
+            print("Present Camera")
+        }
+        alertController.addAction(cameraAction)
+        
+        let photoLibraryAction = UIAlertAction(title: "Photo Library", style: .default) { _ in
+            print("Present Photo Library")
+        }
+        alertController.addAction(photoLibraryAction)
+        
+        let cancelAction = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
+        alertController.addAction(cancelAction)
+        if #available(iOS 16.0, *) {
+            alertController.popoverPresentationController?.sourceItem = sender
+        } else {
+            // Fallback on earlier versions
+            alertController.popoverPresentationController?.barButtonItem = sender
+        }
+        present(alertController, animated: true, completion: nil)
+    }
+    
+    
+    var item: Item! {
+        didSet {
+            navigationItem.title = item.name
+        }
+    }
+    
+    let numberFormatter: NumberFormatter = {
+        let formatter = NumberFormatter()
+        formatter.numberStyle = .decimal
+        formatter.maximumFractionDigits = 2
+        formatter.minimumFractionDigits = 2
+        return formatter
+    }()
+    
+    let dateFormatter: DateFormatter = {
+        let formatter = DateFormatter()
+        formatter.dateStyle = .medium
+        formatter.timeStyle = .none
+        return formatter
+    }()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        nameField.text = item.name
+        serialNumberField.text = item.serialNumber
+        valueField.text = numberFormatter.string(from: NSNumber(value: item.valueInDollars))
+        dateLabel.text = dateFormatter.string(from: item.dateCreated)
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        
+        view.endEditing(true)
+        
+        item.name = nameField.text ?? ""
+        item.serialNumber = serialNumberField.text
+        if let valueText = valueField.text,
+           let value = numberFormatter.number(from: valueText) {
+            item.valueInDollars = value.intValue
+        } else {
+            item.valueInDollars = 0
+        }
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
+    
+}
