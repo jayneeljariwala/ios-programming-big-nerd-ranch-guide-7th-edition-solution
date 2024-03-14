84892a079075e281b34c42c444242179f9babca7
jayneeljariwala <jayneel2001@gmail.com>
2024-03-14 19:54:09 +0530

    Completed chapter 21 silver challenge

---
+++ ch 21 silver/Photorama/Photorama/PhotoCollectionViewCell.swift
@@ -0,0 +1,25 @@
+//
+//  PhotoCollectionViewCell.swift
+//  Photorama
+//
+//  Created by Jayneel Jariwala on 09/03/24.
+//
+
+import UIKit
+
+class PhotoCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet var imageView: UIImageView!
+    @IBOutlet var spinner: UIActivityIndicatorView!
+    
+    func update(displaying image: UIImage?) {
+        if let imageToDisplay = image {
+            spinner.stopAnimating()
+            imageView.image = imageToDisplay
+        } else {
+            spinner.startAnimating()
+            imageView.image = nil
+        }
+    }
+    
+}
